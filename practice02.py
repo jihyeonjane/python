e8ee75101f7e01bfc11b4510c9d6996f02fd1092
Jihyeon <sdj02187@gmail.com>
2024-11-20 22:32:11 +0900

    ver09-4

--- practice02.py
+++ practice02.py
@@ -5,3 +5,7 @@ print("이러면 브랜치가 아주 복잡해져요")
 
 print("version08-1")
 print("version08-2 브랜치 여러개만 하면 이상해져")
+print("version09-1")
+print("version09-2 이번에는 master branch에서 checkout 한 다음에")
+print("version09-3 버전 9 브랜치를 만들었어요. 이렇게 작업해왔기 때문이죠....")
+print("이게 문제가 맞나 보자구요")
