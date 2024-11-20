2ded3f718023ee5607e95b98cdb3a1a8a8185496
Jihyeon <sdj02187@gmail.com>
2024-11-20 23:13:14 +0900

    normal commit

--- practice02.py
+++ practice02.py
@@ -9,4 +9,6 @@ print("version09-1")
 print("version09-2 이번에는 master branch에서 checkout 한 다음에")
 print("version09-3 버전 9 브랜치를 만들었어요. 이렇게 작업해왔기 때문이죠....")
 print("이게 문제가 맞나 보자구요")
+
 print("something wrong....")
+print("그냥 master commit")
